7a938f524d24050f5dc45725557bf892a3d1ea55
HjiaXing97 <17388766232@163.com>
2023-02-22 10:53:53 +0800

    feat:基础配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'),
+			components: resolve(__dirname, 'src/components'),
+			package: resolve(__dirname, 'src/package'),
+			utils: resolve(__dirname, 'src/utils'),
+			store: resolve(__dirname, 'src/store'),
+			assets: resolve(__dirname, 'src/assets'),
+			router: resolve(__dirname, 'src/router'),
+			types: resolve(__dirname, 'src/types')
+		},
+		extensions: ['js', 'json', 'ts', 'vue']
+	}
+});
